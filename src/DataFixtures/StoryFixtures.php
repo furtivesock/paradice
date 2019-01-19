9e4ccaf54a8ee3473a60af634844552aec87bcdd
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-01-19 18:30:49 +0100

    Add DoctrineBundleFixtures

---
+++ src/DataFixtures/StoryFixtures.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Story;
+
+class StoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        
+    }
+}
