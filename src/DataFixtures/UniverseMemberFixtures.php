e54870efccbc07f27c80d7edb4dc17c6fe48ee67
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-01-26 09:45:14 +0100

    UniverseMemberFixtures : add dataset

--- src/DataFixtures/UniverseMemberFixtures.php
+++ src/DataFixtures/UniverseMemberFixtures.php
@@ -4,14 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\UniverseMember;
 
-class UniverseMemberFixtures extends Fixture
+class UniverseMemberFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $universeMember = new UniverseMember();
+        $universeMember->setMember($this->getReference('Sloky'));
+        $universeMember->setUniverse($this->getReference('The-Universe'));
+        $universeMember->setAcceptationDate(new \DateTime());
+        
+        $manager->persist($universeMember);
 
         $manager->flush();
+    }
+
+    public function getDependencies() 
+    {
+        return array(
+            OnlineUserFixtures::class,
+            UniverseFixtures::class
+        );
     }
 }
