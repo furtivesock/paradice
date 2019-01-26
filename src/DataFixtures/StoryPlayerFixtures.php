4891b6ed37017ac07be45b3778430e0d62b6c51c
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-01-26 19:49:24 +0100

    StoryPlayerFixtures : add dataset

--- src/DataFixtures/StoryPlayerFixtures.php
+++ src/DataFixtures/StoryPlayerFixtures.php
@@ -4,14 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\StoryPlayer;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class StoryPlayerFixtures extends Fixture
+class StoryPlayerFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $storyPlayer = new StoryPlayer();
+        $storyPlayer->setPlayer($this->getReference('Xeyh'));
+        $storyPlayer->setStory($this->getReference('SuperStory'));
+        $storyPlayer->setAcceptationDate(new \DateTime());
+
+        $manager->persist($storyPlayer);
 
         $manager->flush();
+    }
+
+    public function getDependencies() 
+    {
+        return array(
+            PersonaFixtures::class,
+            StoryFixtures::class
+        );
     }
 }
