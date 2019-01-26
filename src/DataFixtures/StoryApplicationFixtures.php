e19667a25eed0cf2d5e0786ef5fa9e825ee7ce20
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-01-26 19:43:49 +0100

    StoryApplication : add dataset

--- src/DataFixtures/StoryApplicationFixtures.php
+++ src/DataFixtures/StoryApplicationFixtures.php
@@ -4,14 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\StoryApplication;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-class StoryApplicationFixtures extends Fixture
+class StoryApplicationFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $storyApplication = new StoryApplication();
+        $storyApplication->setApplicant($this->getReference('Xeyh'));
+        $storyApplication->setStory($this->getReference('SuperStory'));
+        $storyApplication->setApplicationDate(new \DateTime());
+        $storyApplication->setMotivation('Je suis motivée de chez mo-ti-vée');
+        $storyApplication->setAccepted(true);
+
+        $manager->persist($storyApplication);
 
         $manager->flush();
+    }
+
+    public function getDependencies() 
+    {
+        return array(
+            PersonaFixtures::class,
+            StoryFixtures::class
+        );
     }
 }
