010434ba0f56248f2180c462113301442fef6190
Quentin <dominguesquentin@gmail.com>
2019-02-01 15:35:26 +0100

    Creation user

--- src/Entity/OnlineUser.php
+++ src/Entity/OnlineUser.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
