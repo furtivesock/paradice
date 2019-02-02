dfe20478b1e02114775b1ebddbdde6a267e15a3b
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-02-02 22:53:12 +0100

    Add action to get top universes as a json response

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Universe;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HomeController extends AbstractController
 {
@@ -38,5 +39,21 @@ class HomeController extends AbstractController
             'topUniversesMonth' => $topUniversesMonth,
             'user' => $this->getUser(),
         ]);
+    }
+
+    /**
+     * @Route("/home/top/{afterDate}", name="home_top")
+     */
+    public function topStoriesJson(\DateTime $afterDate)
+    {
+        $topUniverses = $this->getDoctrine()
+            ->getRepository(Universe::class)
+            ->findTopUniversesAfterDate($afterDate, self::NB_TOP);
+
+        return new JsonResponse(
+            $topUniverses->map(function(Universe $universe) {
+                return $universe->toJson();
+            })->toArray()
+        );
     }
 }
