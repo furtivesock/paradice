a7bce58dfcd0c2462dbfae3d283cd72c265ec8ec
aleryc <aleryc.serrania@etu.parisdescartes.fr>
2019-02-03 16:26:04 +0100

    Add custom queries to get data with predefined order

--- src/Repository/StoryRepository.php
+++ src/Repository/StoryRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\Story;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Query\Expr\OrderBy;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @method Story|null find($id, $lockMode = null, $lockVersion = null)
@@ -19,22 +22,92 @@ class StoryRepository extends ServiceEntityRepository
         parent::__construct($registry, Story::class);
     }
 
-    // /**
-    //  * @return Story[] Returns an array of Story objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Story[] Returns an array of Story objects
+     */
+    public function findStoriesOrderedByLastUpdate(int $idUniverse) : Collection
     {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
+        $results = $this->createQueryBuilder('s')
+            ->addSelect('max(m.creationDate)')
+            ->leftJoin('s.chapters', 'c')
+            ->leftJoin('c.messages', 'm')
+            ->andWhere('s.universe = :id_universe')
+            ->addGroupBy('s')
+            ->addOrderBy(new OrderBy('max(m.creationDate)', 'DESC'))
+            ->setParameter('id_universe', $idUniverse)
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
+
+        $stories = new ArrayCollection();
+        foreach ($results as $result) {
+            $stories[] = $result[0]; // only get Story Entities (without creationDate)
+        }
+
+        return $stories;
     }
-    */
+
+    /**
+     * @return Story[] Returns an array of Story objects
+     */
+    public function findStoriesOrderedByCreationDate(int $idUniverse) : Collection
+    {
+        $results = $this->createQueryBuilder('s')
+            ->andWhere('s.universe = :id_universe')
+            ->addOrderBy(new OrderBy('s.creationDate', 'DESC'))
+            ->setParameter('id_universe', $idUniverse)
+            ->getQuery()
+            ->getResult();
+
+        return new ArrayCollection($results);
+    }
+
+    /**
+     * @return Story[] Returns an array of Story objects
+     */
+    public function findStoriesOrderedByActivity(int $idUniverse, string $order) : Collection
+    {
+
+        switch ($order) {
+            case 'top_day':
+                $date = (new \DateTime())->sub(new \DateInterval('P1D'));
+                break;
+            case 'top_week':
+                $date = (new \DateTime())->sub(new \DateInterval('P1W'));
+                break;
+            case 'top_month':
+                $date = (new \DateTime())->sub(new \DateInterval('P1M'));
+                break;
+            case 'top_year':
+                $date = (new \DateTime())->sub(new \DateInterval('P1D'));
+                break;
+        }
+
+        $results = $this->createQueryBuilder('s')
+            ->addSelect('count(m.id)')
+            ->leftJoin('s.chapters', 'c')
+            ->leftJoin('c.messages', 'm')
+            ->andWhere('s.universe = :id_universe')
+            ->addGroupBy('s')
+            ->addOrderBy(new OrderBy('count(m.id)', 'DESC'))
+            ->setParameter('id_universe', $idUniverse);
+
+        if ($order !== 'top_all') {
+            $results = $results
+                ->andWhere('m.creationDate > :date')
+                ->setParameter('date', $date);
+        }
+
+        $results = $results
+            ->getQuery()
+            ->getResult();
+
+
+        return new ArrayCollection(
+            array_map(function ($result) {
+                return $result[0];
+            }, $results)
+        );
+    }
 
     /*
     public function findOneBySomeField($value): ?Story
@@ -46,5 +119,5 @@ class StoryRepository extends ServiceEntityRepository
             ->getOneOrNullResult()
         ;
     }
-    */
+     */
 }
