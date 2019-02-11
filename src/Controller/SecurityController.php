fd036e3267f271a1e86c913c6d7e10b13c98f883
aleryc <alerycserrania@gmail.com>
2019-02-11 14:44:58 +0100

    Add user to the variable sent into the view

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,6 +26,7 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
+            'user' => $this->getUser(),
             'last_username' => $lastUsername, 
             'error' => $error
         ]);
