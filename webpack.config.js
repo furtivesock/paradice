5a46f962f629f5b0001a63e9e0600b7a49181031
Sofi <chaussettefurtive@outlook.fr>
2019-02-23 03:54:08 +0100

    Add Sass

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
