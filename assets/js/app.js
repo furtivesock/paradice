252f2d2cef16c113cc1855667bc1fc6d938c96d7
Sophie Nguyen <chaussettefurtive@outlook.fr>
2019-04-12 05:12:59 +0200

    Add navbar, modify general bootstrap properties

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+require('bootstrap')
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
+    // Images
+
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]'
+    })
+
     /*
      * ENTRY CONFIG
      *
@@ -18,6 +25,8 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addStyleEntry('css/app', './assets/sass/app.scss')
+    
+    .addEntry('js/app', './assets/js/app.js')
     .addEntry('js/order/stories', './assets/js/order/stories.js')
     .addEntry('js/filter/top-universe', './assets/js/filter/top-universe.js')
     .addEntry('js/chat/messages', './assets/js/chat/messages.js')
@@ -25,9 +34,10 @@ Encore
     .addEntry('js/status/story', './assets/js/status/story.js')
     //.addEntry('js/folder/filename_without_extension', './assets/js/folder/file.js')
     //.addStyleEntry('css/folder/your_cssfile_without_extension', './assets/css/your_file.scss')
+
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -49,7 +59,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
